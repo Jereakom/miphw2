ae07964e483016245d84a666a20c3ced1c8ba683
root <jereakom@gmail.com>
2016-09-12 09:18:40 +0300

    preliminary dependency in package.sjon, modified router.js to include basic routes

--- router.js
+++ router.js
@@ -1,22 +1,77 @@
 var express = require('express');
 var router = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
+
+// read == get
+// write == post
+// update == put
+// delete == delete
+
+// Student routes
 
 router.get('/student', function (req, res) {
   res.send('Student route');
 });
 
+router.post('/student/:id/:name/:address/:class', function (req, res) {
+  res.send(req.params);
+});
+
+router.put('/student/:id/:name/:address/:class', function (req, res) {
+  res.send(req.params);
+});
+
+router.delete('/student/:id', function (req, res) {
+  res.send(req.params);
+});
+
+// Course routes
+
 router.get('/course', function (req, res) {
   res.send('Course route');
 });
 
-router.get('/grades', function (req, res) {
-  res.send('Grades route');
+router.post('/course/:id/:description/:id', function (req, res) {
+  res.send(req.params);
 });
+
+router.put('/course/:id/:description/:id', function (req, res) {
+  res.send(req.params);
+});
+
+router.delete('/course/:id', function (req, res) {
+  res.send(req.params);
+});
+
+// Grade routes
+
+router.get('/grades/:studentID/:courseID/:grade', function (req, res) {
+  res.send(req.params);;
+});
+
+router.post('/grades/:studentID/:courseID/:grade', function (req, res) {
+  res.send(req.params);
+});
+
+router.put('/grades/:studentID/:courseID/:grade', function (req, res) {
+  res.send(req.params);
+});
+
+router.delete('/grades/:studentID/:courseID/', function (req, res) {
+  res.send(req.params);
+});
+
+// Default route
 
 router.get('/', function (req, res) {
   res.send('Default view');
 });
+
+// Redirect to default route
+
+router.get('*',function (req, res) {
+        res.redirect('/');
+    });
 
 router.listen(port);
